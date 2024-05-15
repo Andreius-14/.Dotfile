50132a1fc527d73824de7693813301402e93c36e
Andreius_14 <Andreius_14@outlook.com>
2024-05-15 02:57:21 -0500

    Code ytfzf

---
+++ ytfzf/.config/ytfzf/conf.sh
@@ -0,0 +1,24 @@
+
+# YTFZF_HIST=1  
+# YTFZF_LOOP=0 
+FZF_PLAYER="mpv"
+YTFZF_PREF="bestaudio/best" 
+YTFZF_ENABLE_FZF_DEFAULT_OPTS=1 
+
+# video_pref="bestvideo[height<=?720][fps<=?30]"
+video_pref="bestaudio[height<=?480][fps<=?30]"
+audio_pref="bestaudio/audio"
+
+thumbnail_viewer='chafa'
+# thumbnail_viewer='catimg'
+# thumbnail_viewer='catimg-256'
+
+
+
+enable_back_button=yes
+enable_hist=no
+enable_submenus=yes
+
+skip_thumb_download=no
+show_thumbnails=1
+thumbnail_quality=high
