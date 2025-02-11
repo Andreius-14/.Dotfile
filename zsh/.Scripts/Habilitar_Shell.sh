42e1d584e968895c0c14383b9880c6cd5bccab6c
Andreius_14 <Andreius_14@outlook.com>
2025-02-11 15:53:23 -0500

    Limpieza - Orden

--- zsh/.Scripts/Habilitar_Shell.sh
+++ zsh/.Scripts/Habilitar_Shell.sh
@@ -10,6 +10,7 @@ habilitar_zsh() {
   # Inicio
   if [ -d "$HOME/.oh-my-zsh" ]; then
     echo "Oh-My-Zsh ===> Ya instalado."
+    Zsh_Plugins
 
     # Verificar si está configurado en el archivo .zshrc
     if grep -q "oh-my-zsh" "$HOME/.zshrc"; then
@@ -34,6 +35,11 @@ Zsh_Plugins() {
   txt_color "Instalando Plugins" blue
   # you-should-use
   git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+  # Requiere - fzf
+  git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search
+
 }
 
 
