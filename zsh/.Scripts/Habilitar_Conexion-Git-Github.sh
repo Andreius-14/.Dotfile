be574c77e2e147516c11422e8eb292fbfefc648f
Andreius_14 <Andreius_14@outlook.com>
2025-02-19 01:52:21 -0500

    scrip github SSH

--- zsh/.Scripts/Habilitar_Conexion-Git-Github.sh
+++ zsh/.Scripts/Habilitar_Conexion-Git-Github.sh
@@ -5,6 +5,7 @@ correo=$(input_validador "Ingrese Su Correo Para Ejecutar el Script" email)
 ruta="$HOME/.ssh"
 
 laShell=$(basename $SHELL 2>/dev/null)
+fileName=''
 
 cambiar-shell() {
   __preguntaDeConfirmacion "¿Desea cambiar Tu Shell de $laShell a Zsh?" || return 1
@@ -14,18 +15,16 @@ cambiar-shell() {
 }
 
 creacion-De-Llave() {
-  
   local mensaje=${1:-"¿Desea Crear Nuevas Llaves SSH?"}
   __preguntaDeConfirmacion "$mensaje" || return 1
   ssh-keygen -t ed25519 -C "$correo" || ssh-keygen -t rsa -b 4096 -C "$correo"
-
 }
 
 paso-sshAgent() {
   verifica-Llaves-Existente
 
   txt_color "\nSeleccione el Archivo creado [id_ed25519 o id_rsa] (Sin .pub)" green
-  local fileName=$(__seleccionar_archivo $ruta)
+  fileName=$(__seleccionar_archivo $ruta)
 
   if eval "$(ssh-agent -s)"; then
     txt_color "✔ Agente SSH iniciado correctamente" green
@@ -53,6 +52,7 @@ verifica-Llaves-Existente() {
 #         Main
 # ═══════════════════════════════
 
+# Creando Llaves
 cambiar-shell
 
 if ! verifica-Llaves-Existente; then
@@ -63,5 +63,7 @@ fi
 
 paso-sshAgent
 
+# Mensaje Final 
 txt_color "\nIngrese el siguiente llave a tu cuenta de Github\n" blue
-cat $ruta/id_*.pub
+cat $ruta/$fileName.pub
+txt_color "\nDespues de Pasar tu Key a Github Ejecuta >>> ssh -T git@github.com\n" blue
