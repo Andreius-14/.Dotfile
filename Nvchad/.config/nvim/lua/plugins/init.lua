ffe728ae5d134939236281a03f17e8aa4044e1a8
Andreius_14 <Andreius_14@outlook.com>
2025-02-22 22:00:27 -0500

    Pluggin Activados

--- Nvchad/.config/nvim/lua/plugins/init.lua
+++ Nvchad/.config/nvim/lua/plugins/init.lua
@@ -5,7 +5,7 @@ return {
 	-- ═════════════════════════════════════
 
 	{ "nvzone/volt", lazy = true },
-	{ "nvzone/menu", lazy = true },
+
 	{
 		"nvzone/minty",
 		cmd = { "Shades", "Huefy" },
--- Nvchad/.config/nvim/lua/plugins/menu.lua
+++ Nvchad/.config/nvim/lua/plugins/menu.lua
@@ -1,56 +1,31 @@
--- return {
--- 	"nvzone/menu",
--- 	lazy = true,
--- 	event = "VeryLazy",
--- 	dependencies = {
--- 		{ "nvzone/volt", lazy = true },
--- 		{
--- 			"nvzone/minty",
--- 			cmd = { "Shades", "Huefy" },
--- 		},
--- 		{ "siduck/timerly", cmd = "TimerlyToggle" },
--- 	},
--- 	opts = {
--- 		mouse = true,
--- 		border = true,
--- 	},
--- 	config = function()
--- 		-- Keyboard users
--- 		vim.keymap.set("n", "<C-t>", function()
--- 			require("menu").open("default")
--- 		end, {})
---
--- 		-- mouse users + nvimtree users!
--- 		vim.keymap.set("n", "<RightMouse>", function()
--- 			vim.cmd.exec('"normal! \\<RightMouse>"')
---
--- 			local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
--- 			require("menu").open(options, { mouse = true })
--- 		end, {})
--- 	end,
--- }
-    -- return {
---   {
---     "nvzone/menu",
---     -- lazy = false,
---     config = function()
---       -- Keyboard users
---       vim.keymap.set("n", "<C-t>", function()
---         require("menu").open "default"
---       end, {})
---
---       -- mouse users + nvimtree users!
---       vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
---         require("menu.utils").delete_old_menus()
---
---         vim.cmd.exec '"normal! \\<RightMouse>"'
---
---         -- clicked buf
---         local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
---         local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
---
---         require("menu").open(options, { mouse = true })
---       end, {})
---     end,
---   },
--- }
+return {
+	{
+		"nvzone/menu",
+		lazy = false, -- Carga el plugin al iniciar Neovim
+		dependencies = { "nvzone/volt" }, -- Dependencia requerida
+		config = function()
+
+            -- ######################## CONFIGURACION ################################
+
+			-- Keyboard users
+			vim.keymap.set("n", "<C-t>", function()
+				require("menu").open("default")
+			end, {})
+
+			-- mouse users + nvimtree users!
+			vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+				require("menu.utils").delete_old_menus()
+
+				vim.cmd.exec('"normal! \\<RightMouse>"')
+
+				-- clicked buf
+				local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+				local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+				require("menu").open(options, { mouse = true })
+			end, {})
+
+            -- ######################## CONFIGURACION ################################
+		end,
+	},
+}
