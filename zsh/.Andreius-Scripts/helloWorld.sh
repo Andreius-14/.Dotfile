2c4fb922cfbe8152bf24baf2f3e4b0c861ff545b
Andreius_14 <Andreius_14@outlook.com>
2025-03-08 15:20:17 -0500

    Eliminando Carpeta Original de ShellScript El cual Fue Convertido en Su propio Repositorio

--- zsh/.Andreius-Scripts/helloWorld.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source __FuncionesCompartidas.sh
-
-echo "Hola Mundo desde la terminal"
-txt_color "Instalando Plugins" red
--- zsh/.Andreius-Scripts/test.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bas
-source __FuncionesCompartidas.sh
-
-# __preguntaDeConfirmacion "¿Esta de Color?" || __salir
-# txt_color "Logrado" blue
