8311d6ebb76269d36121e634137fb001890d254b
Andreius_14 <Andreius_14@outlook.com>
2025-03-19 20:09:53 -0500

    Nvchad - LSP

--- Nvchad/.config/nvim/lua/configs/lspconfig.lua
+++ Nvchad/.config/nvim/lua/configs/lspconfig.lua
@@ -1,58 +1,58 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 
 -- EXAMPLE
 local servers = {
 
-	-- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
-	-- ┃          WEB          ┃
-	-- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
+  -- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+  -- ┃          WEB          ┃
+  -- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
 
-	"html",
-	"cssls",
-	-- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
-	-- ┃        PYTHON         ┃
-	-- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
-	-- "pyright",
-	"pylsp",
+  "html",
+  "cssls",
+  -- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+  -- ┃        PYTHON         ┃
+  -- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
+  -- "pyright",
+  "pylsp",
 
-	-- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
-	-- ┃      JAVASCRIPT       ┃
-	-- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
-	"denols",
-	-- "ts_ls",
-	-- "eslint-lsp", Inservible
+  -- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+  -- ┃      JAVASCRIPT       ┃
+  -- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
+  "denols",
+  "ts_ls",
+  -- "eslint-lsp", Inservible
 
-	-- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
-	-- ┃       AVANZADO        ┃
-	-- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
+  -- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+  -- ┃       AVANZADO        ┃
+  -- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
 
-	-- C C++
-	"clangd",
-	-- Bash
-	"bashls",
-	-- Lua
-	"lua_ls",
+  -- C C++
+  "clangd",
+  -- Bash
+  "bashls",
+  -- Lua
+  "lua_ls",
 
-	-- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
-	-- ┃      COMPLEMENTO      ┃
-	-- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
-	-- "emmet-ls",
-	"custom_elements_ls",
-	"jsonls",
+  -- ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+  -- ┃      COMPLEMENTO      ┃
+  -- ┗━━━━━━━━━━━━━━━━━━━━━━━┛
+  -- "emmet-ls",
+  "custom_elements_ls",
+  "jsonls",
 }
 
-local nvlsp = require("nvchad.configs.lspconfig")
+local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = nvlsp.on_attach,
-		on_init = nvlsp.on_init,
-		capabilities = nvlsp.capabilities,
-	})
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
 end
 
 -- configuring single server, example: typescript
