51c5b6d78ec457fb89dd475730118045099431d2
Andreius_14 <Andreius_14@outlook.com>
2025-03-26 00:19:04 -0500

    Nvchad - Cs

--- Nvchad/.config/nvim/lua/mappings.lua
+++ Nvchad/.config/nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require("nvchad.mappings")
+require "nvchad.mappings"
 
 -- add yours here
 
@@ -13,33 +13,34 @@ map("n", "<C-a>", "<cmd>Huefy <cr>") -- Avanzado
 
 -- >>>>>>>>>>>>>> Code-Runner  <<<<<<<<<<<<<<<<
 vim.keymap.set({ "n", "t" }, "<F9>", function()
-	require("nvchad.term").runner({
+  require("nvchad.term").runner {
 
-		-- Modo [01] -- Post Colega
-		id = "code-runner",
-		-- pos = "sp",
-		pos = "vsp",
-		-- pos = "float",
+    -- Modo [01] -- Post Colega
+    id = "code-runner",
+    -- pos = "sp",
+    pos = "vsp",
+    -- pos = "float",
 
-		cmd = function()
-			-- Variables [Rutas con Emogis o ASCII]
-			local file = vim.fn.shellescape(vim.fn.expand("%:p"))
-			local file_wo_ext = vim.fn.shellescape(vim.fn.expand("%:p:r"))
+    cmd = function()
+      -- Variables [Rutas con Emogis o ASCII]
+      local file = vim.fn.shellescape(vim.fn.expand "%:p")
+      local file_wo_ext = vim.fn.shellescape(vim.fn.expand "%:p:r")
 
-			local ft_cmds = {
+      local ft_cmds = {
 
-				python = "python " .. file,
-				cpp = "g++ -std=c++17 -O2 -DHynDuf " .. file .. " -o " .. file_wo_ext,
-				lua = "lua " .. file,
+        python = "python " .. file,
+        cpp = "g++ -std=c++17 -O2 -DHynDuf " .. file .. " -o " .. file_wo_ext,
+        lua = "lua " .. file,
+        cs = "dotnet script" .. file,
 
-				javascript = "node " .. file, -- Comando para JavaScript
-				java = "javac " .. file .. " && java " .. file_wo_ext, -- Comando para Java
-			}
+        javascript = "node " .. file, -- Comando para JavaScript
+        java = "javac " .. file .. " && java " .. file_wo_ext, -- Comando para Java
+      }
 
-			return ft_cmds[vim.bo.ft]
-		end,
-		-- clear_cmd = "",
-	})
+      return ft_cmds[vim.bo.ft]
+    end,
+    -- clear_cmd = "",
+  }
 end)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
