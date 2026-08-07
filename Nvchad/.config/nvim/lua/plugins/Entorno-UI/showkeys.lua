9259571aa1c4f281ba09e4c78303c7a0d927c98e
Andreius_14 <Andreius_14@outlook.com>
2026-08-07 12:58:48 -0500

    nvim _ 3vitar error showkey

--- Nvchad/.config/nvim/lua/plugins/Entorno-UI/showkeys.lua
+++ Nvchad/.config/nvim/lua/plugins/Entorno-UI/showkeys.lua
@@ -2,17 +2,17 @@ return {
 	-- ── showkeys.nvim ───────────────────────────────────────────────────
 	-- Muestra las teclas Pesionadas (https://github.com/nvzone/showkeys)
 	-- ──────────────────────────────────────────────────────────────────────
-	{
-		"nvzone/showkeys",
-		event = "VimEnter", -- ← Carga al iniciar Neovim (con o sin archivo)
-		config = function()
-			-- Activa showkeys automáticamente
-			vim.cmd("ShowkeysToggle")
-		end,
-		opts = {
-			timeout = 1,
-			maxkeys = 5,
-			-- tus opciones
-		},
-	},
+	-- {
+	-- 	"nvzone/showkeys",
+	-- 	event = "VimEnter", -- ← Carga al iniciar Neovim (con o sin archivo)
+	-- 	config = function()
+	-- 		-- Activa showkeys automáticamente
+	-- 		vim.cmd("ShowkeysToggle")
+	-- 	end,
+	-- 	opts = {
+	-- 		timeout = 1,
+	-- 		maxkeys = 5,
+	-- 		-- tus opciones
+	-- 	},
+	-- },
 }
